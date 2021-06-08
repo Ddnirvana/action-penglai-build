e50220bd1e205a923bf3f209444029743180d89a
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-06-08 10:59:03 +0800

    update scripts to manually enter the workdir
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM ddnirvana/penglai-enclave:v0.1
 MAINTAINER Dong Du "Dd_nirvana@sjtu.edu.com"
 
 COPY entrypoint.sh /entrypoint.sh
+COPY Image /Image
 
 ENTRYPOINT ["/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -l
 
 echo PWD: $pwd
-echo entering github workdir
-cd /github/workspace
-scripts/build_opensbi.sh
+echo entering github workdir: /github/workspace
+cd /github/workspace/
+ls
+cd opensbi-0.9
+mkdir -p build-oe/qemu-virt
+# Note: the Image is a placeholder here
+CROSS_COMPILE=riscv64-unknown-linux-gnu- make O=build-oe/qemu-virt PLATFORM=generic FW_PAYLOAD=y FW_PAYLOAD_PATH=/Image
+#scripts/build_opensbi.sh
 #exit 0
